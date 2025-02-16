92dd7625689cfcefcd018b54ee56c1a80188ad16
Pranit Karkera <pranitkarkera99@gmail.com>
2025-02-16 12:54:22 +0530

    rectified db connection issue

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const { initializeDatabase } = require("./db/db");
-const app = express();
 const pollsRoutes = require("./routes/polls.route");
+
+const app = express();
 
 const corsOptions = {
   origin: "*",
@@ -22,11 +23,10 @@ app.use("/api/polls", pollsRoutes);
 
 initializeDatabase()
   .then(() => {
-    const PORT = process.env.PORT || 3000;
-    app.listen(PORT, () => {
-      console.log(`Server is running on ${PORT}`);
-    });
+    console.log("Database connected successfully");
   })
   .catch((error) => {
     console.error("Database connection failed:", error);
   });
+
+module.exports = app;
